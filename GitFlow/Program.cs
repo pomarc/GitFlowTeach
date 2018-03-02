6cfea55081923388b9cca5322848e406b1eab4c5
pomarc <pomarc@gmail.com>
2018-03-02 16:29:18 +0100

    added gitgnore
    to ignore all the vs2017 crap

---
+++ GitFlow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
